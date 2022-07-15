3a5336cec438da9df8126a09d8a819f091e8e8b4
EFSTRATIOS NIKIDIS <100240665+enikidis@users.noreply.github.com>
2022-07-15 23:06:00 +0300

    teest

---
+++ csv2sh.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = pd.read_csv(r'E:\csv_gen\al092004_Ivan.csv')
+print (data)
--- hello.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt 
-
-x= [1,2,14]
-y= [13,24,14]
-
-
-plt.plot(x,y)
-plt.show()
