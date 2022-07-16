116d1c67ce0a549029e834213affa96daa9b13c8
EFSTRATIOS NIKIDIS <100240665+enikidis@users.noreply.github.com>
2022-07-16 03:54:32 +0300

    First version

--- NGOMv19b_al092004_smc_sn_hc.sh
+++ NGOMv19b_al092004_smc_sn_hc.sh
@@ -6,4 +6,71 @@
 #or don't :-) 
 #-------------------------------------------------------------------
 INSTANCENAME=NGOMv19b_al092004_smc_sn_hc
-INSTANCENAME=NGOMv19b_al092004_smc_sn_hc
+ASGSADMIN=archcomputerservice@gmail.com
+RMQMessaging_Enable=off
+RMQMessaging_Transmit=off
+# Input files and templates 
+GRIDNAME=NGOMv19b
+source $SCRIPTDIR/config/mesh_defaults.sh 
+CONTROLTEMPLATE=
+TRIGGER=rssembedded
+RSSSITE=filesystem
+FTPSITE=filesystem
+FDIR=/ddnA/work/$USER/fordir
+HDIR=/ddnA/work/$USER/bestdir
+TIDEFAC=on
+HINDCASTLENGTH=30.0
+BACKGROUNDMET=off
+FORECASTCYCLE=00,06,12,18 
+forecastSelection=strict 
+TROPICALCYCLONE=on
+STORM=09
+YEAR=2004
+WAVES=on
+VARFLUX=off
+CYCLETIMELIMIT=99:00:00
+# Computational Resources (related defaults set in platforms.sh) 
+NCPU=959
+NUMWRITERS=1
+NCPUCAPACITY=9999
+# Post processing and publication
+EMAILNOTIFY=yes
+INTENDEDAUDIENCE=general
+OPENDAPPOST=opendap_post2.sh
+POSTPROCESS=includeWind10m.sh createOPeNDAPFileList.sh $OPENDAPPOST
+OPENDAPNOTIFY=asgs.cera.lsu@coastalrisk.live,archcomputerservice@gmail.com
+NOTIFY_SCRIPT=cera_notify.sh 
+TDS=( lsu_tds ) 
+# Initial state (overridden by STATEFILE after ASGS gets going) 
+HINDCASTENDDATE=2004091100
+HINDCASTLENGTH=30.0
+COLDSTARTDATE=2004081200
+HOTORCOLD=coldstart
+LASTSUBDIR=null
+#
+# Scenario package 
+#
+PERCENT=default 
+SCENARIOPACKAGESIZE=0 
+case $si in 
+   -2)
+       ENSTORM=hindcast
+       ;;
+   -1)
+      # do nothing ... this is not a forecast
+       ENSTORM=nowcast
+       ;;
+    0)
+       ENSTORM=nhcConsensusWind10m
+      source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on Wind10m suffix
+      ;;
+    1)
+       ENSTORM=nhcConsensus
+       ;;
+    *)
+       echo  CONFIGRATION ERROR: Unknown ensemble member number: 
+       ;;
+esac
+#
+PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz 
+HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz 
--- csv2sh.py
+++ csv2sh.py
@@ -1,12 +1,12 @@
 import csv
 import os
-from platform import libc_ver
 from pyparsing import line
 with open('al092004_Ivan.csv') as file:
     reader=csv.DictReader(file)  
     for row in reader:
         shfile=row['INSTANCENAME']
         if os.path.exists(shfile+".sh"):
+            print("Rewriting file")
             os.remove(shfile+".sh")
         else:
             print("The file does not exist")
@@ -23,4 +23,80 @@ with open('al092004_Ivan.csv') as file:
         f.writelines([line1, line2, line3,line4,line5,line6,line7])
         #Start creating variables
         f.write("INSTANCENAME="+row['INSTANCENAME']+"\n")
-        f.write("INSTANCENAME="+row['INSTANCENAME'])
+        f.write("ASGSADMIN="+row['ASGSADMIN']+"\n")
+        #f.write("ACCOUNT="+row['ACCOUNT']+"\n") this need work to the csv
+        f.write("RMQMessaging_Enable="+row['RMQMessaging_Enable']+"\n")
+        f.write("RMQMessaging_Transmit="+row['RMQMessaging_Transmit']+"\n")
+        #f.write("enablePostStatus="+row['enablePostStatus']+"\n") this need work to the csv
+        #f.write("enableStatusNotify="+row['enableStatusNotify']+"\n") this need work to the csv
+        #f.write("statusNotify="+row['statusNotify']+"\n") this need work to the csv
+        f.write("# Input files and templates \n")
+        f.write("GRIDNAME="+row['GRIDNAME']+"\n")
+        f.write("source $SCRIPTDIR/config/mesh_defaults.sh \n")
+        f.write("CONTROLTEMPLATE="+row['CONTROLTEMPLATE']+"\n")
+        f.write("TRIGGER="+row['TRIGGER']+"\n")
+        f.write("RSSSITE="+row['RSSSITE']+"\n")
+        f.write("FTPSITE="+row['FTPSITE']+"\n")
+        f.write("FDIR="+row['FDIR']+"\n")
+        f.write("HDIR="+row['HDIR']+"\n")
+        f.write("TIDEFAC="+row['TIDEFAC']+"\n")
+        f.write("HINDCASTLENGTH="+row['HINDCASTLENGTH']+"\n")
+        f.write("BACKGROUNDMET="+row['BACKGROUNDMET']+"\n")
+        f.write("FORECASTCYCLE=00,06,12,18 \n")
+        f.write("forecastSelection=strict \n")
+        f.write("TROPICALCYCLONE="+row['TROPICALCYCLONE']+"\n")
+        f.write("STORM="+row['STORM']+"\n")
+        f.write("YEAR="+row['YEAR']+"\n")
+        f.write("WAVES="+row['WAVES']+"\n")
+        f.write("VARFLUX="+row['VARFLUX']+"\n")
+        f.write("CYCLETIMELIMIT="+row['CYCLETIMELIMIT']+"\n")
+        f.write("# Computational Resources (related defaults set in platforms.sh) \n")
+        f.write("NCPU="+row['NCPU']+"\n")
+        f.write("NUMWRITERS="+row['NUMWRITERS']+"\n")
+        f.write("NCPUCAPACITY="+row['NCPUCAPACITY']+"\n")
+        f.write("# Post processing and publication\n")
+        f.write("EMAILNOTIFY="+row['EMAILNOTIFY']+"\n")
+        f.write("INTENDEDAUDIENCE="+row['INTENDEDAUDIENCE']+"\n")
+        f.write("OPENDAPPOST="+row['OPENDAPPOST']+"\n")
+        f.write("POSTPROCESS="+row['POSTPROCESS']+"\n")
+        f.write("OPENDAPNOTIFY="+row['OPENDAPNOTIFY']+"\n")
+        f.write("NOTIFY_SCRIPT=cera_notify.sh \n")
+        f.write("TDS=( lsu_tds ) \n")
+        f.write("# Initial state (overridden by STATEFILE after ASGS gets going) \n")
+        f.write("HINDCASTENDDATE="+row['HINDCASTENDDATE']+"\n")
+        f.write("HINDCASTLENGTH="+row['HINDCASTLENGTH']+"\n")
+        f.write("COLDSTARTDATE="+row['COLDSTARTDATE']+"\n")
+        f.write("HOTORCOLD="+row['HOTORCOLD']+"\n")
+        f.write("LASTSUBDIR="+row['LASTSUBDIR']+"\n")
+        f.write("#\n")
+        f.write("# Scenario package \n")
+        f.write("#\n")
+        f.write("PERCENT=default \n")
+        f.write("SCENARIOPACKAGESIZE=0 \n")
+        f.write("case $si in \n")
+        f.write("   -2)\n")
+        f.write("       ENSTORM=hindcast\n")
+        f.write("       ;;\n")
+        f.write("   -1)\n")
+        f.write("      # do nothing ... this is not a forecast\n")
+        f.write("       ENSTORM=nowcast\n")
+        f.write("       ;;\n")
+        f.write("    0)\n")
+        f.write("       ENSTORM=nhcConsensusWind10m\n")
+        f.write("      source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on Wind10m suffix\n")
+        f.write("      ;;\n")
+        f.write("    1)\n")
+        f.write("       ENSTORM=nhcConsensus\n")
+        f.write("       ;;\n")
+        f.write("    *)\n")
+        conferror1=("       echo ")
+        conferror2=(" CONFIGRATION ERROR: Unknown ensemble member number: \n ")  #this must be added to the csv file. there is conflict btween the character for strings from bash and python
+        f.write(conferror1 + conferror2 )
+        
+        
+        
+        f.write("      ;;\n")
+        f.write("esac\n")
+        f.write("#\n")
+        f.write("PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz \n")
+        f.write("HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz \n")
