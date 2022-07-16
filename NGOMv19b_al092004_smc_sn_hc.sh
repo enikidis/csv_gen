e28a2b425c58db983051b8009224324e0f528e5e
EFSTRATIOS NIKIDIS <100240665+enikidis@users.noreply.github.com>
2022-07-16 03:02:19 +0300

    First try

---
+++ NGOMv19b_al092004_smc_sn_hc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+#N O T E: 
+#This configuration file was generated automatically from a CSV file. 
+#Any changes should probably be managed via the method of creation rather 
+#than by manually editing this file ... unless you know what you're doing, 
+#or don't :-) 
+#-------------------------------------------------------------------
+INSTANCENAME=NGOMv19b_al092004_smc_sn_hc
+INSTANCENAME=NGOMv19b_al092004_smc_sn_hc
--- csv2sh.py
+++ csv2sh.py
@@ -1,3 +1,26 @@
-import pandas as pd
-data = pd.read_csv(r'E:\csv_gen\al092004_Ivan.csv')
-print (data)
+import csv
+import os
+from platform import libc_ver
+from pyparsing import line
+with open('al092004_Ivan.csv') as file:
+    reader=csv.DictReader(file)  
+    for row in reader:
+        shfile=row['INSTANCENAME']
+        if os.path.exists(shfile+".sh"):
+            os.remove(shfile+".sh")
+        else:
+            print("The file does not exist")
+        f = open(shfile+".sh", "x")
+        #Indicator for generated file that is a bash script.
+        line1 ="#!/bin/sh \n"
+        #Start writing static things
+        line2="#N O T E: \n"
+        line3="#This configuration file was generated automatically from a CSV file. \n"
+        line4="#Any changes should probably be managed via the method of creation rather \n"
+        line5="#than by manually editing this file ... unless you know what you're doing, \n"
+        line6="#or don't :-) \n"
+        line7="#-------------------------------------------------------------------\n"
+        f.writelines([line1, line2, line3,line4,line5,line6,line7])
+        #Start creating variables
+        f.write("INSTANCENAME="+row['INSTANCENAME']+"\n")
+        f.write("INSTANCENAME="+row['INSTANCENAME'])
